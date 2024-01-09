fb6c730f9e87a770d9779780b937e1027896f9d0
ArturVRSampaio <artur.vitorio@astisoftware.com.br>
2024-01-09 10:34:56 -0300

    more collumns

--- sendToDatabase.py
+++ sendToDatabase.py
@@ -23,41 +23,40 @@ cursor = connection.cursor()
 
 # Abrir o arquivo output.json como um arquivo de texto
 with open('output.json', 'r', encoding='utf-8') as file:
-    # Carregar dados JSON como uma lista
-    data_list = json.load(file)
+    # Carregar dados JSON como um dicionário
+    data_dict = json.load(file)
 
-    # Iterar sobre cada dicionário na lista
-    for app_dict in data_list:
-        # Iterar sobre as chaves (IDs de aplicativos) no dicionário
-        for app_id, app_data in app_dict.items():
-            # Substituir ou remover caracteres não suportados na coluna 'name'
-            app_data["name"] = re.sub(r'[^\x00-\x7F]+', '', app_data["name"])
-            app_data["developer"] = re.sub(r'[^\x00-\x7F]+', '', app_data["developer"])
-            app_data["publisher"] = re.sub(r'[^\x00-\x7F]+', '', app_data["publisher"])
+    # Iterar sobre cada chave (ID de aplicativo) no dicionário
+    for app_id, app_data in data_dict.items():
+        # Substituir ou remover caracteres não suportados nas colunas 'name', 'developer', 'publisher'
+        app_data["name"] = re.sub(r'[^\x00-\x7F]+', '', app_data["name"])
+        app_data["developer"] = re.sub(r'[^\x00-\x7F]+', '', app_data["developer"])
+        app_data["publisher"] = re.sub(r'[^\x00-\x7F]+', '', app_data["publisher"])
 
-            # Construir a consulta SQL para inserir os dados na tabela
-            query = f"""
-                INSERT INTO steam_spy_data
-                (appid, name, developer, publisher, score_rank, positive, negative, userscore,
-                owners, average_forever, average_2weeks, median_forever, median_2weeks,
-                price, initialprice, discount, ccu)
-                VALUES
-                (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
-            """
+        # Construir a consulta SQL para inserir os dados na tabela
+        query = f"""
+            INSERT INTO steam_spy_data
+            (appid, name, developer, publisher, score_rank, positive, negative, userscore,
+            owners, average_forever, average_2weeks, median_forever, median_2weeks,
+            price, initialprice, discount, ccu, languages, genre, tags)
+            VALUES
+            (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+        """
 
-            # Obter os valores dos dados do aplicativo
-            values = (
-                app_data.get("appid", None), app_data.get("name", None), app_data.get("developer", None),
-                app_data.get("publisher", None), app_data.get("score_rank", None), app_data.get("positive", None),
-                app_data.get("negative", None), app_data.get("userscore", None), app_data.get("owners", None),
-                app_data.get("average_forever", None), app_data.get("average_2weeks", None),
-                app_data.get("median_forever", None), app_data.get("median_2weeks", None),
-                app_data.get("price", None), app_data.get("initialprice", None), app_data.get("discount", None),
-                app_data.get("ccu", None)
-            )
+        # Obter os valores dos dados do aplicativo
+        values = (
+            app_data.get("appid", None), app_data.get("name", None), app_data.get("developer", None),
+            app_data.get("publisher", None), app_data.get("score_rank", None), app_data.get("positive", None),
+            app_data.get("negative", None), app_data.get("userscore", None), app_data.get("owners", None),
+            app_data.get("average_forever", None), app_data.get("average_2weeks", None),
+            app_data.get("median_forever", None), app_data.get("median_2weeks", None),
+            app_data.get("price", None), app_data.get("initialprice", None), app_data.get("discount", None),
+            app_data.get("ccu", None), app_data.get("languages", None),
+            app_data.get("genre", None), json.dumps(app_data.get("tags", {}))
+        )
 
-            # Executar a consulta SQL
-            cursor.execute(query, values)
+        # Executar a consulta SQL
+        cursor.execute(query, values)
 
 # Confirmar as alterações no banco de dados
 connection.commit()
