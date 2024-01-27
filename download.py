0f806f8070daf59a59aa7f963f8bb8aba0cc1e66
ArturVRSampaio <artur.vitorio@astisoftware.com.br>
2024-01-27 16:22:11 -0300

    update download to multithread

--- download.py
+++ download.py
@@ -1,11 +1,27 @@
 import requests
 import json
+from concurrent.futures import ThreadPoolExecutor
+import time
 
 url = "https://steamspy.com/api.php"
-
 all_data = []
 
-# download all games basic info
+
+def get_game_details(item):
+    print("item :" + item)
+    params = {"request": "appdetails", "appid": item}
+    response = requests.get(url, params=params)
+
+    if response.status_code == 200:
+        data = response.json()
+        with open("output.json", "a", encoding="utf-8") as file:
+            file.write(f'"{item}": {json.dumps(data, ensure_ascii=False, indent=4)},\n')
+    else:
+        print(f"Erro ao fazer a solicitação para a página {item}. Código de status: {response.status_code}")
+
+
+start_time = time.time()
+
 for page in range(0, 70):
     print("page " + str(page))
     params = {"request": "all", "page": page}
@@ -14,27 +30,19 @@ for page in range(0, 70):
 
     if response.status_code == 200:
         data = response.json()
-
-        all_data.append(data)
+        all_data.extend(data)
     else:
         print(f"Erro ao fazer a solicitação para a página {page}. Código de status: {response.status_code}")
 
-
 with open("output.json", "w", encoding="utf-8") as file:
     file.write("{")
-    for page in all_data:
-        for item in page:
-            print("item " + str(item))
-            file.write("\"" + str(item) + "\"" + " : ")
-            params = {"request": "appdetails", "appid": item}
-            response = requests.get(url, params=params)
 
-            if response.status_code == 200:
-                data = response.json()
-                json.dump(data, file, ensure_ascii=False, indent=4)
-                file.write(",")
+with ThreadPoolExecutor() as executor:
+    executor.map(get_game_details, all_data)
 
-            else:
-                print(f"Erro ao fazer a solicitação para a página {item}. Código de status: {response.status_code}")
-
+with open("output.json", "a", encoding="utf-8") as file:
     file.write("}")
+
+end_time = time.time()
+total_time = end_time - start_time
+print(f"Total time spent: {total_time} seconds")
--- sendToDatabase.py
+++ sendToDatabase.py
@@ -9,31 +9,24 @@ db_user = os.getenv("MYSQL_USER")
 db_password = os.getenv("MYSQL_PASSWORD")
 db_database = os.getenv("MYSQL_DATABASE")
 
-# Conectar ao banco de dados MySQL
 connection = mysql.connector.connect(
     user=db_user,
     password=db_password,
     database=db_database,
     port='8005',
-    charset='utf8mb4',  # Adicione isso para suportar uma gama mais ampla de caracteres
+    charset='utf8mb4',
 )
 
-# Criar um cursor para executar consultas SQL
 cursor = connection.cursor()
 
-# Abrir o arquivo output.json como um arquivo de texto
 with open('output.json', 'r', encoding='utf-8') as file:
-    # Carregar dados JSON como um dicionário
     data_dict = json.load(file)
 
-    # Iterar sobre cada chave (ID de aplicativo) no dicionário
     for app_id, app_data in data_dict.items():
-        # Substituir ou remover caracteres não suportados nas colunas 'name', 'developer', 'publisher'
         app_data["name"] = re.sub(r'[^\x00-\x7F]+', '', app_data["name"])
         app_data["developer"] = re.sub(r'[^\x00-\x7F]+', '', app_data["developer"])
         app_data["publisher"] = re.sub(r'[^\x00-\x7F]+', '', app_data["publisher"])
 
-        # Construir a consulta SQL para inserir os dados na tabela
         query = f"""
             INSERT INTO steam_spy_data
             (appid, name, developer, publisher, score_rank, positive, negative, userscore,
@@ -43,7 +36,6 @@ with open('output.json', 'r', encoding='utf-8') as file:
             (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
         """
 
-        # Obter os valores dos dados do aplicativo
         values = (
             app_data.get("appid", None), app_data.get("name", None), app_data.get("developer", None),
             app_data.get("publisher", None), app_data.get("score_rank", None), app_data.get("positive", None),
@@ -55,12 +47,9 @@ with open('output.json', 'r', encoding='utf-8') as file:
             app_data.get("genre", None), json.dumps(app_data.get("tags", {}))
         )
 
-        # Executar a consulta SQL
         cursor.execute(query, values)
 
-# Confirmar as alterações no banco de dados
 connection.commit()
 
-# Fechar o cursor e a conexão
 cursor.close()
 connection.close()
